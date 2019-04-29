60ed3aa6218965adeed901cb874bde6b0748038d
SnowleopardB <b.j.g.akerboom@gmail.com>
2019-04-29 14:24:59 +0200

    Fourth version

--- app/src/main/java/com/example/friendstr/MainActivity.java
+++ app/src/main/java/com/example/friendstr/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.friendstr;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,8 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String[] friendsnames = {"Ada", "Arthur", "Curly", "Finn", "Freddie", "Grace", "John", "Micheal", "Polly", "Tommy"};
-        String[] friendsbios = {"I am not a bloody Shelby", "Wash it down with a nice drink!", "No hart in motorcars", "I can't do it!", "The revolution is near!", "Now you've seen me", "Fucking waps", "Men and their cocks never cease to amaze me.", "I'm not a fucking kid anymore", "I do what I do to protect my family"};
+        String[] friendsbios = {"I am not a bloody Shelby", "Wash it down with a nice drink!", "No hart in motorcars", "I can't do it!", "The revolution is near!", "Now you've seen me", "Fucking waps", "I'm not a fucking kid anymore", "Men and their cocks never cease to amaze me.", "I do what I do to protect my family"};
 
+        SharedPreferences prefs = getSharedPreferences("settings", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
 
         for (int i = 0; i < friendsnames.length; i++){
             int photoID = (int) getResources().getIdentifier(friendsnames[i].toLowerCase(), "drawable", getPackageName());
@@ -31,15 +34,15 @@ public class MainActivity extends AppCompatActivity {
         FriendsAdapter adapter = new FriendsAdapter(this, R.layout.grid_item, friends);
         GridView grid_view = findViewById(R.id.gridview);
         grid_view.setAdapter(adapter);
+        GridItemClickListener onclick = new GridItemClickListener();
+        grid_view.setOnItemClickListener(onclick);
     }
+
 
     private class GridItemClickListener implements AdapterView.OnItemClickListener {
 
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            GridItemClickListener listen = new GridItemClickListener();
-            GridView grid_view = findViewById(R.id.gridview);
-            grid_view.setOnItemClickListener(listen);
             Friend clickedFriend = (Friend) parent.getItemAtPosition(position);
             Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
             intent.putExtra("clicked_friend", clickedFriend);
--- app/src/main/java/com/example/friendstr/ProfileActivity.java
+++ app/src/main/java/com/example/friendstr/ProfileActivity.java
@@ -1,20 +1,24 @@
 package com.example.friendstr;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    Friend retrievedFriend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_profile);
         Intent intent = getIntent();
-        Friend retrievedFriend = (Friend) ((Intent) intent).getSerializableExtra("clicked_friend");
+        retrievedFriend = (Friend) ((Intent) intent).getSerializableExtra("clicked_friend");
+
 
         ImageView image = (ImageView) findViewById(R.id.imageView2);
         image.setImageResource(retrievedFriend.getDrawableId());
@@ -23,8 +27,23 @@ public class ProfileActivity extends AppCompatActivity {
         name.setText(retrievedFriend.getName());
 
         TextView bio = (TextView) findViewById(R.id.textView3);
-        bio.setText(retrievedFriend.getName());
+        bio.setText(retrievedFriend.getBio());
 
+        RatingBar rating = (RatingBar) findViewById(R.id.ratingBar);
+        rating.setOnRatingBarChangeListener(new Ratingbarchange());
+        SharedPreferences prefs = getSharedPreferences("settings", MODE_PRIVATE);
+        float stars = prefs.getFloat(retrievedFriend.getName(), 0);
+        rating.setRating(stars);
+
+    }
+    class Ratingbarchange implements RatingBar.OnRatingBarChangeListener{
+        @Override
+        public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+            SharedPreferences.Editor editor = getSharedPreferences("settings", MODE_PRIVATE).edit();
+            String name = retrievedFriend.getName();
+            editor.putFloat(name, rating);
+            editor.commit();
+        }
     }
 
 }
